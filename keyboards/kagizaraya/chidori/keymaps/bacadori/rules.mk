2f90098bb677c349622ae89ecc497825a045d2e8
bacafreak <76938009+bacafreak@users.noreply.github.com>
2022-09-23 14:00:02 +1000

    update chidori

--- keyboards/kagizaraya/chidori/keymaps/bacadori/rules.mk
+++ keyboards/kagizaraya/chidori/keymaps/bacadori/rules.mk
@@ -8,7 +8,7 @@ BOOTLOADER = usbasploader
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no        # Mouse keys
+MOUSEKEY_ENABLE = yes        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
@@ -16,13 +16,14 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-CAPS_WORD_ENABLE = yes
+CAPS_WORD_ENABLE = no
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
 CUSTOM_MATRIX = lite
 WPM_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
+LTO_ENABLE = yes
 
 # project specific files
 SRC += matrix.c
